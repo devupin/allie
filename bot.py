dc8925fb9e19dc28c7d2e4a785415aa619ded81d
avi <hi@avi.im>
2016-10-23 19:08:05 +0530

    Notify admins whenever user is successfuly onboarded

--- bot.py
+++ bot.py
@@ -9,6 +9,25 @@ with open('template.txt') as f:
     template = f.read()
 
 
+def _get_admin_ids():
+    users = sc.api_call('users.list')
+    return [u['id'] for u in users['members'] if u.get('is_admin') is True]
+
+
+def get_admin_dm_channels():
+    global admin_dm_channels
+    for admin_id in _get_admin_ids():
+        response = sc.api_call('im.open', user=admin_id)
+        admin_dm_channels.append(response['channel']['id'])
+    return admin_dm_channels
+
+
+def notify_admin(username):
+    for dm_channel_id in admin_dm_channels:
+        m = 'User: {} successfully on-boarded'.format(username)
+        sc.rtm_send_message(channel=dm_channel_id, message=m)
+
+
 def send_welcome_message(user):
     user_id = user['id']
     response = sc.api_call('im.open', user=user_id)
@@ -18,6 +37,7 @@ def send_welcome_message(user):
         print('Shite happened')
         return
     sc.rtm_send_message(channel=dm_channel_id, message=template)
+    notify_admin(username=user['name'])
 
 
 def main():
@@ -33,4 +53,5 @@ def main():
 
 
 if __name__ == '__main__':
+    get_admin_dm_channels()
     main()

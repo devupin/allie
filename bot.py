62e825bef3779fb9a1aaedd75a24580ee5484462
avi <hi@avi.im>
2016-10-26 13:11:17 +0530

    Add some logging messages

--- bot.py
+++ bot.py
@@ -37,20 +37,23 @@ def send_welcome_message(user):
     try:
         dm_channel_id = response['channel']['id']
     except (KeyError, ValueError):
-        print('Shite happened')
+        print("Shite happened: {}".format(slack_name))
         return
     message = template.format(real_name=real_name, slack_name=slack_name)
     sc.rtm_send_message(channel=dm_channel_id, message=message)
     notify_admin(username=user['name'])
+    print("Sent onboarding message to {}".format(slack_name))
 
 
 def main():
     if sc.rtm_connect():
+        print('---Connected to Slack successfully---')
         while True:
             for event in sc.rtm_read():
                 if event.get('type') == team_join_event and (
                         event['user']['is_bot'] is False):
                     send_welcome_message(user=event['user'])
+            # imma sleep for a while
             time.sleep(1)
     else:
         print('Connection Failed, invalid token?')
